89ad4fa289996559978a3589e46e56304ee2327b
StevenNdaye <stevenndaye@gmail.com>
2016-07-19 22:46:18 +0200

    Updated settings in the yaml as well as the mainClasses

---
+++ specification/src/main/java/com/vatit/specification/Application.java
@@ -0,0 +1,9 @@
+package com.vatit.specification;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
---
+++ standard/src/main/java/com/vatit/standard/Application.java
@@ -0,0 +1,9 @@
+package com.vatit.standard;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
