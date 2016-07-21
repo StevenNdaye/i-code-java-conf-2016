f455f3ea65a2b8c6bbd81c490a6974e70ed8f1ab
StevenNdaye <stevenndaye@gmail.com>
2016-07-21 23:13:46 +0200

    Code cleanup

--- query_dsl/src/main/java/com/vatit/querydsl/repository/CustomerRepository.java
+++ query_dsl/src/main/java/com/vatit/querydsl/repository/CustomerRepository.java
@@ -2,13 +2,13 @@ package com.vatit.querydsl.repository;
 
 import com.vatit.querydsl.entities.Customer;
 import com.vatit.querydsl.entities.QCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer>,
+public interface CustomerRepository extends JpaRepository<Customer, Integer>,
         QueryDslPredicateExecutor<Customer>,
         QuerydslBinderCustomizer<QCustomer> {
 
