4f82a5d2e21eb267bcc5966aabc6d4a1da405363
StevenNdaye <stevenndaye@gmail.com>
2016-07-21 20:39:53 +0200

    Added a preloaded sql file

---
+++ standard/src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (1, 'Steven', 'Ndaye', '1987-06-26', '2007-01-13', 0);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (2, 'Aslam', 'Khan', '1988-01-20', '2007-02-13', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (3, 'Tsatsi', 'Mahase', '1987-01-11', '2007-03-13', 1);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (4, 'Thato', 'Morobane', '1987-03-24', '2007-02-14', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (5, 'Charlene', 'Tshitoka', '1990-06-26', '2007-01-20', 1);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (6, 'Claude', 'Mwamba', '1987-04-04', '2007-05-13', 0);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (7, 'Jonah', 'Kuria', '1987-01-20', '2007-05-18', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (8, 'Orville', 'Khangale', '1986-06-30', '2007-03-20', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (9, 'Ted', 'Cruz', '1966-06-26', '2007-01-09', 0);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (10, 'Clinton', 'Hillary', '1954-06-20', '2007-04-19', 1);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (11, 'Donald', 'Trump', '1988-11-21', '2007-03-25', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (12, 'Jacob', 'Zuma', '1985-11-11', '2007-01-16', 0);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (13, 'Lindie', 'Kruger', '1989-12-12', '2008-02-13', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (14, 'Grace', 'Kibambe', '1982-06-06', '2007-03-20', 2);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (15, 'Stephanie', 'Muka', '1981-06-29', '2007-01-29', 1);
+INSERT INTO customer (id, first_name, last_name, birth_date, enrollment, status)
+VALUES (16, 'Steven', 'Ndaye', '1987-06-26', '2007-01-13', 1);
